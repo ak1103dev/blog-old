e15b26dca563fab5a16294603bf3dcdb85c829a7
ak1103dev <ak.apichan1103@gmail.com>
2019-07-28 00:25:28 +0700

    :rocket: can deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/blog",
   siteMetadata: {
     title: `AK1103DEV's blog`,
     description: `ak1103dev is a blogger, developer, and investor`,
@@ -26,8 +27,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `AK1103DEV's Blog`,
+        short_name: `blog`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
@@ -44,6 +45,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
